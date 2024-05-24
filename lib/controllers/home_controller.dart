18409e99173a718c2d7c8d872abd54fc838cbfa3
Xolmatov Foziljon <xolmatovfozil9@gmail.com>
2024-05-24 18:05:36 +0300

    service and page

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,4 +1,3 @@
-import 'package:examgetx/pages/second_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
@@ -7,12 +6,14 @@ import 'package:get/get.dart';
 
 class HomeController extends GetxController{
 
-  callSecondPage(BuildContext context){
-    Get.to(SecondPage());
+  callSecondPage(BuildContext context ,){
+    //Get.to(SecondPage());
   //   Navigator.push(context, MaterialPageRoute(
   //       builder: (BuildContext context) {
   //       return SecondPage();
   //   }));
 
    }
+
+
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:examgetx/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../services/http_service.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,8 +12,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  late Future<List<Article>> items;
   final homeController = Get.find<HomeController>();
-
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    items = NewsService().fetchTopHeadlines()
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +30,26 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.orange,
       ),
       body: GetBuilder<HomeController>(
-        builder: (context){
-          return Center(
-            child: Text("Something",style: TextStyle(color: Colors.orange),),
+
+        builder: (context,){
+          return ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  leading: articles[index].urlToImage != null
+                      ? Image.network(
+                    articles[index].urlToImage,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  )
+                      : null,
+                  title: Text(articles[index].title),
+                  subtitle: Text(articles[index].description),
+                  onTap: () => _openArticle(context, articles[index].url),
+                ),
+              );
+            },
           );
         },
       ),
@@ -44,3 +69,9 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+
+// Center(
+// child: Text("Something",style: TextStyle(color: Colors.orange),),
+// );
